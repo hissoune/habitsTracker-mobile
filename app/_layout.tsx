86b45f3d1ff06094f0b14d7a58c8b26c7162a597
hissoune <khalidhissoune962@gmail.com>
2025-02-17 19:21:45 +0100

    [Layout] Updated main app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,12 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { Provider } from 'react-redux';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import store from './(redux)/store';
+import AppWrapper from './(redux)/AppWrapper';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -28,14 +30,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="auth" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+    <Provider store={store}>
+       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <AppWrapper/>
+   
       <StatusBar style="auto" />
     </ThemeProvider>
+    </Provider>
+   
   );
 }
