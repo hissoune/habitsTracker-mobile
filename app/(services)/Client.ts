bad56160c9086e8d8d698180ad32dc731bdcf0ee
hissoune <khalidhissoune962@gmail.com>
2025-02-20 09:21:07 +0100

    [Service] Enhanced Client service for token management

---
+++ app/(services)/Client.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_URL,
+
+});
+axiosInstance.interceptors.request.use(
+ async function (config) {
+  
+    const token =await AsyncStorage.getItem('token');
+    
+  
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    
+    }
+    
+    
+      config.headers['Content-Type'] = 'application/json';
+   
+
+    return config;
+  },
+  function (error) {
+     
+    return Promise.reject(error); 
+  }
+);
+
+export default axiosInstance;
