dc447fc6b99454e41e3e4bc0d3e811286df0f649
hissoune <khalidhissoune962@gmail.com>
2025-02-24 10:12:56 +0000

    [Redux] Updated store configuration and inject the habitReducer

--- app/(redux)/store.ts
+++ app/(redux)/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from './aithSlice';
+import { habitReducer } from './hapitSlice';
 
 const store = configureStore({
     reducer: {
-        auth: authReducer
+        auth: authReducer,
+        habit:habitReducer
     }
 });
 
