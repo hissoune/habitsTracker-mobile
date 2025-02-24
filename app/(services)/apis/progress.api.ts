2125bb8a51bc64b91f185609646ba74619490759
hissoune <khalidhissoune962@gmail.com>
2025-02-24 21:42:34 +0000

    [API] Updated progress API

--- app/(services)/apis/progress.api.ts
+++ app/(services)/apis/progress.api.ts
@@ -4,5 +4,6 @@ import axiosInstance from "../Client";
 
 export const getProgress = async (habitId:string)=>{
    const response = await axiosInstance.get(`progress/${habitId}`);
+   
    return response.data
 }
