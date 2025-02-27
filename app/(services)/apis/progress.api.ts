9472658e65e67d96284e7c92e6339f2b38c062b3
hissoune <khalidhissoune962@gmail.com>
2025-02-27 19:00:36 +0000

    Updated progress API calls to sync with backend changes

--- app/(services)/apis/progress.api.ts
+++ app/(services)/apis/progress.api.ts
@@ -3,7 +3,13 @@ import axiosInstance from "../Client";
 
 
 export const getProgress = async (habitId:string)=>{
-   const response = await axiosInstance.get(`progress/${habitId}`);
+   const response = await axiosInstance.get(`habits-service/progress/${habitId}`);
+   
+   return response.data
+}
+
+export const completProgress = async (progressId:string)=>{
+    const response = await axiosInstance.patch(`habits-service/progress/compleet/${progressId}`);
    
    return response.data
 }
