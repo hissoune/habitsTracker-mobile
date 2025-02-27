f85dc6dc2c4b618f44d8c19be1a5726abf246e71
hissoune <khalidhissoune962@gmail.com>
2025-02-27 19:00:35 +0000

    Refactored auth API service for better authentication flow

--- app/(services)/apis/authApi.ts
+++ app/(services)/apis/authApi.ts
@@ -5,7 +5,7 @@ import axiosInstance from "../Client";
 
 export const register = async (user:User)=>{
     
-    const response = await axiosInstance.post('/auth/register',user);
+    const response = await axiosInstance.post('auth-service/auth/register',user);
 
     return response.data
 
@@ -13,7 +13,7 @@ export const register = async (user:User)=>{
 
 export const login = async (user: { email: string; password: string }) => {
     try {
-      const response = await axiosInstance.post('/auth/login', user);
+      const response = await axiosInstance.post('auth-service/auth/login', user);
       console.log("Login Response: ", response);
       return response.data;
     } catch (error) {
