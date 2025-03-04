adbb41456ac864d6f392e69eea01941dcac9f022
hissoune <khalidhissoune962@gmail.com>
2025-03-04 23:21:45 +0000

    [ADD] Add chalengesApi.ts for challenges API integration

---
+++ app/(services)/apis/chalengesApi.ts
@@ -0,0 +1,14 @@
+
+import axiosInstance from "../Client";
+
+
+
+
+
+
+export const getAllChalenges =async ()=>{
+ 
+    const response = await axiosInstance.get('chalenges-service/chalenges');
+    return response.data
+
+}
