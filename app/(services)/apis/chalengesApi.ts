672e23fb202bcff1905d71ff1303fd1ca127f385
hissoune <khalidhissoune962@gmail.com>
2025-03-06 19:10:04 +0000

    [UPDATE] Update chalengesApi.ts for enhanced API integration

--- app/(services)/apis/chalengesApi.ts
+++ app/(services)/apis/chalengesApi.ts
@@ -12,3 +12,10 @@ export const getAllChalenges =async ()=>{
     return response.data
 
 }
+
+export const joinChalenge = async (chalengeId:string)=>{
+    console.log(chalengeId);
+    
+    const response = await axiosInstance.patch(`chalenges-service/chalenges/join-chalenge/${chalengeId}`);
+    return response.data
+}
