8c99ceefc7322482007a25d39f38f61636c7616e
hissoune <khalidhissoune962@gmail.com>
2025-03-10 12:21:34 +0000

    Introduced common styles file to maintain design consistency.

---
+++ components/ui/comonStyles.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+
+
+export const ComonStyles = StyleSheet.create({
+    loaderContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      },
+})
